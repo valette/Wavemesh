d27c47e824163fe0a98f546de63ebd189a18829f
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2013-05-06 02:12:48 +0200

    initial vector compression code

---
+++ VectorCompression/decodeCoeffs.cxx
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "io.h"
+
+int main( int argc, char *argv[] )
+{
+	int vectorSize = 32;
+	int quantization = 8;
+	char *outputFile = 0;
+
+	if(argc < 1)
+	{
+		std::cout << "Usage :" << std::endl;
+		std::cout << "decodeCoeffs inputfile" << std::endl;	
+		return (0);
+	}
+
+	char* verificationFile = 0;
+
+	// Parse optionnal arguments
+	int ArgumentsIndex = 2;
+	while (ArgumentsIndex < argc) {
+		if (strcmp(argv[ArgumentsIndex],"-v")==0) {
+			verificationFile = argv[ArgumentsIndex + 1];
+		}
+		if (strcmp(argv[ArgumentsIndex],"-o")==0) {
+			outputFile = argv[ArgumentsIndex + 1];
+		}
+		ArgumentsIndex+=2;
+	}
+
+	coeffs result = decode(argv[1]);
+	if (outputFile) {
+		writeFile(result, outputFile);
+	} else {
+		writeFile(result, "decoded.txt");
+	}
+
+	if (verificationFile) {
+		coeffs reference = readFile(verificationFile, vectorSize);
+		double maxError = 0;
+		for (int i = 0; i != reference.size(); i++) {
+			coefficient tempCoeff = reference[i];
+			for (int j = 0; j != tempCoeff.size(); j++) {
+				double error = fabs(tempCoeff[j] - result[i][j]);
+				if (maxError < error) {
+					maxError = error;
+				}
+			}
+		}
+		std::cout << "Maximum error : " << maxError << std::endl;
+	}
+}
+
---
+++ VectorCompression/encodeCoeffs.cxx
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "io.h"
+
+int main( int argc, char *argv[] )
+{
+	int vectorSize = 32;
+	int quantization = 8;
+	char *outputFile = 0;
+
+
+	if(argc < 1)
+	{
+		std::cout << "Usage :" << std::endl;
+		std::cout << "encode inputfile" << std::endl;	
+		return (0);
+	}
+
+	// Parse optionnal arguments
+	int ArgumentsIndex = 2;
+	while (ArgumentsIndex < argc) {
+		if (strcmp(argv[ArgumentsIndex],"-q")==0) {
+			quantization = atoi(argv[ArgumentsIndex + 1]);
+		}
+		if (strcmp(argv[ArgumentsIndex],"-o")==0) {
+			outputFile = argv[ArgumentsIndex + 1];
+		}
+		ArgumentsIndex+=2;
+	}
+
+	std::cout << "Quantization : " << quantization << " bits" << std::endl;
+	coeffs input = readFile(argv[1], vectorSize);
+	std::cout << input.size() << " coefficients" << std::endl;
+	if (outputFile) {
+		encode(input, outputFile, quantization);
+	} else {
+		encode(input, "compressed.svc", quantization);
+	}
+}
+
